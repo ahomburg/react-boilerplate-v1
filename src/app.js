76a4d7e85703acd5907f8498601c465e4a4fd746
alex homburg <alex.homburg@gmail.com>
2019-10-10 08:48:53 +0200

    rumgespielt

--- src/app.js
+++ src/app.js
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<h1>Boilerplate</h1>, document.getElementById('app'));
+const jsx = 
+(
+    <div>
+        <h1>Boilerplate</h1>
+        <h2>Version 1.0</h2>
+    </div>
+);
+  
+ReactDOM.render(jsx, document.getElementById('app'));
